114932f30ffc93f18d3dba33151ddac75948a13a
joachimbbp <104856283+joachimbbp@users.noreply.github.com>
2024-09-11 13:43:59 -0400

    markdown to plaintext testing

--- src/functions.py
+++ src/functions.py
@@ -10,8 +10,7 @@ def clean_md_line(md):
 
 def summarize(md_path):
     #just cleaning it now to debug
-    summarizer = pipeline("summarization")
-    print("CLEANING")
+    summarizer = pipeline("summarization", model="Falconsai/text_summarization")
     cleaned = ""
     with open(md_path, 'r') as md:
         for line in md:
@@ -20,13 +19,15 @@ def summarize(md_path):
     max_input_length = 1024
     truncated_cleaned = cleaned[:max_input_length] #TODO make this somehow longer, or include salient parts, perhaps manual parsing
 
-    max = min(int(len(truncated_cleaned) / 4), max_input_length)
-    if max < 5:
-        max = 5 #you can clean this code up tbh
-        #INVESTIGATE You should consider increasing `max_length` or, better yet, setting `max_new_tokens`
 
-    print(f"max: {max}, cleaned: {len(cleaned)}")
+    # if max < 5:
+    #     max = 5 #you can clean this code up tbh
+    #     #INVESTIGATE You should consider increasing `max_length` or, better yet, setting `max_new_tokens`
+
+    # #print(f"max: {max}, cleaned: {len(cleaned)}")
 
     #TODO Token indices sequence length is longer than the specified maximum sequence length for this model (1995 > 1024). Running this sequence through the model will result in indexing errors 
-    summary = summarizer(truncated_cleaned, max_length=max, min_length=1, do_sample=False)
+    summary = summarizer(cleaned, max_length=130, min_length=30, do_sample=False)
     return summary[0]['summary_text']
+
+
